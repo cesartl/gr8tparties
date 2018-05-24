392e1ab8ec4d8e8106da3707ced3bb23332051ac
Cesar TL <cesar.tronlozai@gmail.com>
2018-05-24 08:53:29 +0100

    Mongo for Party Service

--- party-service/src/main/java/com/ctl/gr8tparties/model/Party.java
+++ party-service/src/main/java/com/ctl/gr8tparties/model/Party.java
@@ -1,14 +1,27 @@
 package com.ctl.gr8tparties.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by Cesar on 15/01/2017.
  */
+@Document
 public class Party {
+    @Id
     private String id;
+
     private Set<String> guests = new HashSet<>();
+
+    public static Party forGuest(Collection<String> guests){
+        var party = new Party();
+        party.guests.addAll(guests);
+        return party;
+    }
 
     public String getId() {
         return id;
